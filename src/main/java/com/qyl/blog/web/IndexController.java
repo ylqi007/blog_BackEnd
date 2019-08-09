d80c8ada014101f424b9ca2411abd7262c26dee2
ylqi007 <yunlongqi@my.unt.edu>
2019-08-09 18:37:27 -0500

    20190809, LogAspect handle log

--- src/main/java/com/qyl/blog/web/IndexController.java
+++ src/main/java/com/qyl/blog/web/IndexController.java
@@ -3,18 +3,25 @@ package com.qyl.blog.web;
 import com.qyl.blog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
-//        int i = 9 / 0;
+        int i = 9 / 0;
 
         String blog = null;
         if(blog == null) {
             throw  new NotFoundException("博客不春在");
         }
+        return "index";
+    }
+
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name) {
+        System.out.println("----- index -----");
         return "index";
     }
 }
