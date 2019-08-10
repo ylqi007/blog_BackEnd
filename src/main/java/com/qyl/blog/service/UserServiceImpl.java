4e6a3b4b36d90d8afa00a956a2c4f00700cbde6b
ylqi007 <yunlongqi@my.unt.edu>
2019-08-10 13:02:34 -0500

    20190810, background login with MD5

--- src/main/java/com/qyl/blog/service/UserServiceImpl.java
+++ src/main/java/com/qyl/blog/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.qyl.blog.service;
 
 import com.qyl.blog.dao.UserRepository;
 import com.qyl.blog.po.User;
+import com.qyl.blog.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User checkUser(String username, String password) {
-        User user = userRepository.findByUsernameAndPassword(username, password);
+        User user = userRepository.findByUsernameAndPassword(username, MD5Utils.code(password));
         return user;
     }
 }
