ce4dbe2b09462c39249cc63f0d1a5030aa6b659b
ylqi007 <yunlongqi@my.unt.edu>
2019-08-14 09:57:35 -0500

    20190814, manage blogs, edit & delete

--- src/main/java/com/qyl/blog/po/Blog.java
+++ src/main/java/com/qyl/blog/po/Blog.java
@@ -193,6 +193,33 @@ public class Blog {
         this.comments = comments;
     }
 
+    public void init() {
+        this.tagIds = tagsToIds(this.getTags());
+    }
+
+    // "1,2,3"
+    private String tagsToIds(List<Tag> tags) {
+        if(!tags.isEmpty()) {
+            StringBuffer ids = new StringBuffer();
+            boolean flag = false;
+            for(Tag tag: tags) {
+                if(flag) {
+                    ids.append(",");
+                } else {
+                    flag = true;
+                }
+                ids.append(tag.getId());
+            }
+            return ids.toString();
+        } else {
+            return tagIds;
+        }
+    }
+
+
+
+
+
     @Override
     public String toString() {
         return "Blog{" +
--- src/main/java/com/qyl/blog/web/admin/BlogController.java
+++ src/main/java/com/qyl/blog/web/admin/BlogController.java
@@ -14,6 +14,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -53,10 +54,10 @@ public class BlogController {
         return "admin/blogs :: blogList";
     }
 
+    // 新增博客
     @GetMapping("/blogs/input")
     public String input(Model model) {
-        model.addAttribute("types", typeService.listType());    // Initialize types.
-        model.addAttribute("tags", tagService.listTag());    // Initialize types.
+        setTypeAndTag(model);
         model.addAttribute("blog", new Blog());
         return INPUT;
     }
@@ -73,5 +74,28 @@ public class BlogController {
             attributes.addFlashAttribute("message", "操作成功");
         }
         return REDIRECT_LIST;
+    }
+
+    // 更新
+    @GetMapping("/blogs/{id}/input")
+    public String editInput(@PathVariable Long id, Model model) {
+        setTypeAndTag(model);
+        Blog blog = blogService.getBlog(id);
+        blog.init();
+        model.addAttribute("blog", blog);
+        return INPUT;
+    }
+
+    // 删除
+    @GetMapping("/blogs/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes attributes) {
+         blogService.deleteBlog(id);
+         attributes.addFlashAttribute("message", "删除成功");
+         return REDIRECT_LIST;
+    }
+
+    private void setTypeAndTag(Model model) {
+        model.addAttribute("types", typeService.listType());    // Initialize types.
+        model.addAttribute("tags", tagService.listTag());    // Initialize types.
     }
 }
