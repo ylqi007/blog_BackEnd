ae0e615912bad8c23e19f2bc78346a54d62ea1ae
ylqi007 <yunlongqi@my.unt.edu>
2019-08-14 23:10:58 -0500

    20190814, front management, show detail of blog, part 1

--- src/main/java/com/qyl/blog/web/IndexController.java
+++ src/main/java/com/qyl/blog/web/IndexController.java
@@ -39,18 +39,19 @@ public class IndexController {
         return "index";
     }
 
-    @GetMapping("/blog")
-    public String blog() {
-        System.out.println("----- /blog ------");
-        return "blog";
-    }
-
     @PostMapping("/search")
     public String serach(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
                         @RequestParam String query, Model model) {
         model.addAttribute("page", blogService.listBlog("%" + query + "%", pageable));
         model.addAttribute("query", query);
         return "search";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blog(@PathVariable Long id, Model model) {
+        model.addAttribute("blog", blogService.getBlog(id));
+        System.out.println("----- /blog ------");
+        return "blog";
     }
 
 
